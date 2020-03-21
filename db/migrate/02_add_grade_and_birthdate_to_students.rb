53fabab9037226c6c2651dbfdf80b9208554186a
Kirsten Keels <keelsk@gmail.com>
2020-03-21 00:41:22 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,6 +1,8 @@
 class AddGradeAndBirthdateToStudents < ActiveRecord::Migration[5.1]
-  add_column :students do |col|
-    col.integer :grade
-    col.string :birthdate
+  def change  
+    add_column :students do |col|
+      col.integer :grade
+      col.string :birthdate
+    end
   end
 end
